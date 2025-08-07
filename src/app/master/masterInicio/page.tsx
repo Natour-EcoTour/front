c61e554503b42094243d1751692cbdbb3001d559
Flamingo Lindo <vitorantunes2003@gmail.com>
2025-08-07 19:47:25 -0300

    feat: navbar always opened

--- src/app/master/masterInicio/page.tsx
+++ src/app/master/masterInicio/page.tsx
@@ -2,43 +2,43 @@ import Navbar from "@/components/Navbar/Navbar";
 import HomeButton from "@/components/HomeButton/HomeButton";
 export default function MasterUsersPage() {
     return (
-        <div>
+        <div className="ml-80">
             <Navbar />
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center mt-10 px-4 max-w-6xl mx-auto">
                 <HomeButton
                     name="Usuários"
-                    description="Página de usuários do sistema"
+                    description="Adiministre os usuários do sistema"
                     href="/master/masterUsersPage"
                 />
 
                 <HomeButton
-                    name="Usuários"
-                    description="Página de usuários do sistema"
+                    name="Pontos"
+                    description="Administre os pontos dos usuários do sistema"
                     href="/master/masterUsersPage"
                 />
 
                 <HomeButton
-                    name="Usuários"
-                    description="Página de usuários do sistema"
+                    name="Avaliações"
+                    description="Avalições dos pontos"
                     href="/master/masterUsersPage"
                 />
 
                 <HomeButton
-                    name="Usuários"
-                    description="Página de usuários do sistema"
+                    name="Termos e Política"
+                    description="Pagina de administração de termos e política"
                     href="/master/masterUsersPage"
                 />
 
                 <HomeButton
-                    name="Usuários"
-                    description="Página de usuários do sistema"
+                    name="Configurações"
+                    description="Configurações da conta"
                     href="/master/masterUsersPage"
                 />
 
                 <HomeButton
-                    name="Usuários"
-                    description="Página de usuários do sistema"
+                    name="Sair"
+                    description="Sair do sistema"
                     href="/master/masterUsersPage"
                 />
             </div>
--- src/app/master/masterUsersPage/page.tsx
+++ src/app/master/masterUsersPage/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar/Navbar";
 export default function MasterUsersPage() {
     return (
-        <div>
+        <div className="ml-80">
             <Navbar />
             <h1>Master Users Page</h1>
         </div>
